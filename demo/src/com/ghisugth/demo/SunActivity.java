32e81ae236e890a50d1f86f98b8b35e1a8f29150
Alexander Fedora <alexander.fedora@gmail.com>
2012-11-09 00:41:36 -0800

    old api level support in sunlight demo

--- demo/src/com/ghisugth/demo/SunActivity.java
+++ demo/src/com/ghisugth/demo/SunActivity.java
@@ -8,25 +8,36 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 
-public class SunActivity extends Activity implements ActionBar.TabListener {
+public class SunActivity extends Activity {
     private GLSurfaceView glSurfaceView;
+    private TabHandler tabHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Tab tab;
-        ActionBar actionBar = getActionBar();
+        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
 
-        actionBar.setTitle(R.string.title);
-        actionBar.setSubtitle(R.string.subtitle);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        if (currentApiVersion >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            Tab tab;
+            ActionBar actionBar = getActionBar();
 
-        tab = actionBar.newTab();
-        tab.setTabListener(this);
-        tab.setText(R.string.tab_test);
-        tab.setTag(new Test(this));
-        actionBar.addTab(tab);
+            actionBar.setTitle(R.string.title);
+            actionBar.setSubtitle(R.string.subtitle);
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+            tabHandler = new TabHandler(this);
+
+            tab = actionBar.newTab();
+            tab.setTabListener(tabHandler);
+            tab.setText(R.string.tab_test);
+            tab.setTag(new Test(this));
+            actionBar.addTab(tab);
+        } else {
+            glSurfaceView = new Test(this);
+            setContentView(glSurfaceView);
+        }
+
     }
 
     @Override
@@ -45,17 +56,32 @@ public class SunActivity extends Activity implements ActionBar.TabListener {
         }
     }
 
-    @Override
-    public void onTabReselected(Tab tab, FragmentTransaction ft) {
-    }
-
-    @Override
-    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-    }
-
-    @Override
-    public void onTabSelected(Tab tab, FragmentTransaction fragmentTransaction) {
-        glSurfaceView = (GLSurfaceView) tab.getTag();
+    public void setSurfaceView(GLSurfaceView glSurfaceView) {
+        this.glSurfaceView = glSurfaceView;
         setContentView(glSurfaceView);
     }
+
+    private class TabHandler implements ActionBar.TabListener {
+        private SunActivity activity;
+
+        public TabHandler(SunActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        }
+
+        @Override
+        public void onTabSelected(Tab tab, FragmentTransaction fragmentTransaction) {
+            GLSurfaceView glSurfaceView = (GLSurfaceView) tab.getTag();
+            activity.setSurfaceView(glSurfaceView);
+        }
+    }
+
+
 }
