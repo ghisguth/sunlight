31b8a7abb207cfe6b915a90e000292b92914c381
Alexander Fedora <alexander.fedora@gmail.com>
2012-12-24 19:20:17 -0800

    tuning shaders

--- gfx/res/raw/sun_corona_fragment.glsl
+++ gfx/res/raw/sun_corona_fragment.glsl
@@ -8,6 +8,9 @@ varying vec2 vTextureCoord3;
 varying vec2 vTextureCoord4;
 uniform sampler2D sTexture;
 uniform sampler2D sTexture2;
+uniform sampler2D sTexture3;
+uniform sampler2D sTexture4;
+varying float vLevel;
 
 float luminance(vec4 color)
 {
@@ -17,29 +20,24 @@ float luminance(vec4 color)
 
 void main() {
   vec4 sample = texture2D(sTexture, vTextureCoord);
-  vec4 sample2 = texture2D(sTexture2, vTextureCoord2);
-  vec4 sample3 = texture2D(sTexture2, vTextureCoord3);
-  vec4 sample4 = texture2D(sTexture2, vTextureCoord4);
-
+  vec4 sample2 = texture2D(sTexture3, vTextureCoord2);
+  vec4 sample3 = texture2D(sTexture3, vTextureCoord3);
+  vec4 sample4 = texture2D(sTexture3, vTextureCoord4);
   //vec4 color = sample*sample2 + (sample3 * sample3.a) + (sample4 * sample4.a);
   //gl_FragColor = sample*sample2 + (sample3 + sample4)*2.0;
-
-  float noise = 1.25*pow(sample3.r * sample4.g, 2.0);
-
+  float noise = pow(sample3.r * sample4.g, 2.0);
   //float noise = smoothstep(0.30, 0.7, sample3.r * sample4.g);
   //float noise2 = smoothstep(0.05, 0.3, sample3.b);
 
   //gl_FragColor = sample + vec4(noise,noise,noise,0);
   //gl_FragColor = vec4(noise,noise,noise,0);
-
   float noise3 = smoothstep(0.7, 0.9, sample2.r);
   vec4 baseColor = sample  + vec4(noise3,noise3,noise3,0);
   vec4 noiseVec = vec4(noise,noise,noise,0);
-
-  float noise4 = 0.9 * smoothstep(0.1, 0.3, sample3.r * sample4.g);
-
-  gl_FragColor = (baseColor+noiseVec) * (noise4 + 1.0);
-  //gl_FragColor = sample;
+  gl_FragColor = baseColor+noiseVec;
+  float noise4 = smoothstep(0.1, 0.3, sample3.r * sample4.g);
+  //gl_FragColor.rgb = vec3(noise4, noise4, noise4);
+  gl_FragColor.a = noise4 * vLevel + 0.3;//sample2.g * vLevel;
 
   //gl_FragColor = (sample + vec4(noise, noise, noise, 0))*vec4(noise2, noise2, noise2, 1);
   //gl_FragColor = vec4(noise2, noise2, noise2, 1);
--- gfx/res/raw/sun_corona_vertex.glsl
+++ gfx/res/raw/sun_corona_vertex.glsl
@@ -6,6 +6,7 @@ uniform mat4 uMVPMatrix;
 uniform float uAnimationTime;
 uniform float uAnimationTime2;
 uniform float uAnimationTime3;
+uniform float uLevel;
 attribute vec4 aPosition;
 attribute vec2 aTextureCoord;
 varying vec4 vColor;
@@ -13,13 +14,15 @@ varying vec2 vTextureCoord;
 varying vec2 vTextureCoord2;
 varying vec2 vTextureCoord3;
 varying vec2 vTextureCoord4;
+varying float vLevel;
 
 void main() {
     vColor = vec4(1,1,1,1);//normalize(aPosition);//+0.2*vec4(0.5 + aTextureCoord.x * 0.5, 0.5 + aTextureCoord.y * 0.5, 0, 1));
     vTextureCoord = aTextureCoord;
     vTextureCoord2 = aTextureCoord + vec2(-uAnimationTime, 0.0);
     vTextureCoord3 = (aTextureCoord + vec2(-uAnimationTime2, 0.0)) * 2.0;
-    vTextureCoord4 = (aTextureCoord + vec2(uAnimationTime3, 0.0)) * 8.0;
+    vTextureCoord4 = (aTextureCoord + vec2(uAnimationTime3, 0.0)) * 6.0;
     gl_Position = uMVPMatrix * aPosition;
     gl_PointSize = 4.0;
+    vLevel = uLevel;
 }
