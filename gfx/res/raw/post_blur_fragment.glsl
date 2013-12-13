b49fbc52609f079828bd79821a887b5642728da8
Alexander Fedora <alexander.fedora@gmail.com>
2013-12-13 01:11:10 -0800

    lines with single frame buffer implemented

--- gfx/res/raw/post_blur_fragment.glsl
+++ gfx/res/raw/post_blur_fragment.glsl
@@ -1,9 +1,9 @@
 precision mediump float;
 
-uniform float blur;
+uniform float uBlur;
 varying vec2 vTextureCoord;
 uniform sampler2D sTexture;
 
 void main() {
-  gl_FragColor = texture2D(sTexture, vTextureCoord) * vec4(1,1,1,blur);
+  gl_FragColor = texture2D(sTexture, vTextureCoord) * vec4(1,1,1,uBlur);
 }
