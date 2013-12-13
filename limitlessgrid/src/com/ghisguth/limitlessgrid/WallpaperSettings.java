7c7b83466c415ed2d9f79b4c9d0be804e495891e
Alexander Fedora <alexander.fedora@gmail.com>
2013-12-13 05:33:54 -0800

    grid wallpaper ported

---
+++ limitlessgrid/src/com/ghisguth/limitlessgrid/WallpaperSettings.java
@@ -0,0 +1,44 @@
+/**
+ * This file is a part of sunlight project
+ * Copyright (c) $today.year sunlight authors (see file `COPYRIGHT` for the license)
+ */
+
+package com.ghisguth.limitlessgrid;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import cxa.gridwallpaper.R;
+
+public class WallpaperSettings extends PreferenceActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        getPreferenceManager().setSharedPreferencesName(
+                Wallpaper.SHARED_PREF_NAME);
+
+        addPreferencesFromResource(R.xml.settings);
+
+        getPreferenceManager().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        getPreferenceManager().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+    }
+
+}
