0c555f5fd45495abe4658eaff9fc6354dae71961
Alexander Fedora <alexander.fedora@gmail.com>
2015-10-10 14:21:02 -0700

    Clean up code

--- gfx/src/com/ghisguth/gfx/GeometryHelper.java
+++ gfx/src/com/ghisguth/gfx/GeometryHelper.java
@@ -32,7 +32,7 @@ public class GeometryHelper {
     }
 
     public static VertexBuffer createCube(int resolution) {
-        if(resolution < 1) {
+        if (resolution < 1) {
             throw new RuntimeException("resolution cannot be less than 1");
         }
 
@@ -44,21 +44,19 @@ public class GeometryHelper {
         final int otherSideVerticesCount = (resolution - 1) * (resolution - 1);
 
         final int verticesCount = 2 * capVerticesCount + 2 * sideVerticesCount + 2 * otherSideVerticesCount;
-        final int indicesCount = 0;//horizontalResolution * 2 * (verticalResolution - 1);
+        final int indicesCount = 0;
 
         float[] vertices = new float[verticesCount * 5];
         short[] indices = new short[indicesCount];
 
         int index = 0;
 
-        for(int j = 0; j < resolution + 1; ++j)
-        {
-            float v = ((float)j) / resolution;
+        for (int j = 0; j < resolution + 1; ++j) {
+            float v = ((float) j) / resolution;
             float y = -0.5f + v;
 
-            for(int i = 0; i < resolution + 1; ++i)
-            {
-                float u = ((float)i) / resolution;
+            for (int i = 0; i < resolution + 1; ++i) {
+                float u = ((float) i) / resolution;
                 float x = -0.5f + u;
 
                 vertices[index + 0] = x;
@@ -78,14 +76,12 @@ public class GeometryHelper {
 
         index += capVerticesCount * 5;
 
-        for(int j = 1; j < resolution; ++j)
-        {
-            float v = ((float)j) / resolution;
+        for (int j = 1; j < resolution; ++j) {
+            float v = ((float) j) / resolution;
             float y = -0.5f + v;
 
-            for(int i = 0; i < resolution + 1; ++i)
-            {
-                float u = ((float)i) / resolution;
+            for (int i = 0; i < resolution + 1; ++i) {
+                float u = ((float) i) / resolution;
                 float x = -0.5f + u;
 
                 vertices[index + 0] = x;
@@ -105,14 +101,12 @@ public class GeometryHelper {
 
         index += sideVerticesCount * 5;
 
-        for(int j = 1; j < resolution; ++j)
-        {
-            float v = ((float)j) / resolution;
+        for (int j = 1; j < resolution; ++j) {
+            float v = ((float) j) / resolution;
             float y = -0.5f + v;
 
-            for(int i = 1; i < resolution; ++i)
-            {
-                float u = ((float)i) / resolution;
+            for (int i = 1; i < resolution; ++i) {
+                float u = ((float) i) / resolution;
                 float x = -0.5f + u;
 
                 vertices[index + 0] = -0.5f;
@@ -129,52 +123,6 @@ public class GeometryHelper {
                 index += 5;
             }
         }
-
-        /*
-
-        int index = 0;
-        float radius = 1.0f;
-
-        for (int j = 0; j < verticalResolution; ++j) {
-            double v = (double) j / (verticalResolution - 1);
-            double theta = v * Math.PI;
-            double sinTheta = Math.sin(theta);
-            double cosTheta = Math.cos(theta);
-
-            for (int i = 0; i < horizontalResolution; ++i) {
-                double u = (double) i / (horizontalResolution - 1);
-
-                double phi = 2.0f * u * Math.PI;
-                double sinPhi = Math.sin(phi);
-                double cosPhi = Math.cos(phi);
-
-                vertices[index + 0] = (float) (radius * sinTheta * cosPhi);
-                vertices[index + 1] = (float) (radius * sinTheta * sinPhi);
-                vertices[index + 2] = (float) (radius * cosTheta);
-                vertices[index + 3] = (float) (u);
-                vertices[index + 4] = (float) (v);
-                index += 5;
-            }
-        } */
-
-        /*index = 0;
-
-        for (int j = 0; j < verticalResolution - 1; ++j) {
-            boolean dir = (j & 1) != 0;
-            if (dir || true) {
-                for (int i = 0; i < horizontalResolution; ++i) {
-                    indices[index + 0] = (short) (j * horizontalResolution + i);
-                    indices[index + 1] = (short) ((j + 1) * horizontalResolution + i);
-                    index += 2;
-                }
-            } else {
-                for (int i = horizontalResolution - 1; i >= 0; --i) {
-                    indices[index + 0] = (short) (j * horizontalResolution + i);
-                    indices[index + 1] = (short) ((j + 1) * horizontalResolution + i);
-                    index += 2;
-                }
-            }
-        } */
 
         return new VertexBuffer(vertices, indices, true);
     }
