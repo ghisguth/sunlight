65343364c5979f7ff21d3854f0dbfd96d182f7bb
Alexander Fedora <alexander.fedora@gmail.com>
2016-07-17 22:30:58 -0700

    Allow to enable/disable double tap in blured lines wallpaper

--- blurredlines/src/com/ghisguth/blurredlines/Wallpaper.java
+++ blurredlines/src/com/ghisguth/blurredlines/Wallpaper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.ghisguth.wallpaper.GLES20WallpaperService;
 import com.ghisguth.wallpaper.glwallpaperservice.GLWallpaperService;
@@ -21,7 +22,9 @@ public class Wallpaper extends GLES20WallpaperService {
                 Context.MODE_PRIVATE));
     }
 
-    class WallpaperEngine extends GLWallpaperService.GLEngine {
+    class WallpaperEngine extends GLWallpaperService.GLEngine implements SharedPreferences.OnSharedPreferenceChangeListener {
+        private boolean doubleTapEnabled = true;
+
         public WallpaperEngine(Context context, SharedPreferences preferences) {
             super();
 
@@ -32,11 +35,18 @@ public class Wallpaper extends GLES20WallpaperService {
             renderer.setSharedPreferences(preferences);
             setRenderer(renderer);
             setRenderMode(RENDERMODE_CONTINUOUSLY);
+
+            preferences.registerOnSharedPreferenceChangeListener(this);
+            onSharedPreferenceChanged(preferences, null);
         }
 
 
         public Bundle onCommand(java.lang.String action, int x, int y, int z,
                                 android.os.Bundle extras, boolean resultRequested) {
+            if(!this.doubleTapEnabled) {
+                return null;
+            }
+
             Intent myIntent = new Intent();
 
             long currentTime = System.currentTimeMillis();
@@ -53,6 +63,15 @@ public class Wallpaper extends GLES20WallpaperService {
                 }
             }
             return null;
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            try {
+                this.doubleTapEnabled = sharedPreferences.getBoolean("double_tab_settings", true);
+            } catch (final Exception e) {
+                Log.e(TAG, "PREF init error: " + e);
+            }
         }
     }
 }
